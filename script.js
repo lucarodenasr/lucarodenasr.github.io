7486d4196bc99f6ce23a5094623006cb97d127cc
Luca2700 <lucarodenas7@gmail.com>
2025-11-21 23:33:31 +0100

    Version 1.1

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ let date = document.getElementById("dateProyecto")
 const descripciones = {
     'mannad': 'Aplicación móvil en React Native para la gestión de gastos e ingresos y agregar notas, pensado para usarlo en un proyecto personal.',
     'auto': 'Pequeño programa para PC hecho en python para automatizar la eliminacion de archivos, mediante extension de archivo y seleccionando una carpeta.',
-    'ajedrez': 'Proyecto de Ajedrez online con estilo minimalista, Backend en PHP y Frontend en JavaScript.'
+    'ajedrez': 'Proyecto de Ajedrez online con estilo minimalista, Backend en PHP y Frontend en JavaScript.',
+    'oalarma': 'Aplicación móvil en React Native para activar una alarma de una forma personalizada, pensado para usarlo en un proyecto personal.'
 };
 
 const dates = {
     'mannad': '2025',
     'auto': '2023',
-    'ajedrez': '2024'
+    'ajedrez': '2024',
+    'oalarma': '2025'
 };
 
 function selectProjects(opt) {

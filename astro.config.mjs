daf42ff06e46e1ee8b2cd862d66b5523aecf8ecb
Luca2700 <lucarodenas7@gmail.com>
2026-05-07 14:33:43 +0200

    fix node version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config'
-import githubPages from '@astrojs/github-pages'
 
 export default defineConfig({
   site: 'https://lucarodenasr.github.io',
-  integrations: [githubPages()]
 })
